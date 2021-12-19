d0736c364712dc2fbe8b06ea4349ed94532a684c
ewsgit <ews2909@outlook.com>
2021-12-19 19:03:24 +0000

    loading and saving a directory as project

---
+++ install.sh
@@ -0,0 +1,14 @@
+echo "This script requires sudo privileges. Please enter your password to continue."
+su
+echo "Installing ShellUtils..."
+mkdir ~/.ShellUtils/
+cp ./* ~/.ShellUtils/
+cd ~/.ShellUtils/
+npm install
+npm install -g
+echo "Installing Github CLI..."
+sudo apt update
+sudo apt install gh -y
+echo ""
+echo "ShellUtils installed"
+echo "use \"s help\" to see all commands. Hope you enjoy :)"
---
+++ update.sh
@@ -0,0 +1,17 @@
+echo "This script requires sudo privileges. Please enter your password to continue."
+su
+echo Updating Github Cli...
+sudo apt update
+sudo apt install gh -y
+echo Updating ShellUtils...
+cp ~/.ShellUtils/* ~/.ShellUtils-old/
+rm -rf ~/.ShellUtils/
+gh repo clone ewsgit/ShellUtils ~/.ShellUtils/
+cp ~/.ShellUtils-old/settings.json ~/.ShellUtils/settings.json
+cp ~/.ShellUtils-old/scripts/* ~/.ShellUtils/scripts/
+cp ~/.ShellUtils-old/dotfileconf.json ~/.ShellUtils/dotfileconf.json
+cd ~/.ShellUtils/
+npm install
+npm install -g
+echo ""
+echo "ShellUtils update complete, enjoy!"
